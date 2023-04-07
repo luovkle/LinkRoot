12933cb412619e3a3996882b177f0c4529ebb38b
luovkle <roberto.silva.dev@protonmail.com>
2023-04-06 18:03:23 -0600

    ✨ Add get_db dependency implementation

--- backend/app/app/api/api_v1/endpoints/links.py
+++ backend/app/app/api/api_v1/endpoints/links.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Path
+from pymongo.database import Database
 
-from app.api.deps import get_user
+from app.api.deps import get_user, get_db
 from app.schemas.link import LinkCreate, LinkRead, LinkUpdate
 from app.schemas.message import Message
 
@@ -8,29 +9,43 @@ router = APIRouter()
 
 
 @router.post("/@me", response_model=LinkRead)
-def create_link(user: str = Depends(get_user), *, link: LinkCreate):
+def create_link(
+    user: str = Depends(get_user), db: Database = Depends(get_db), *, link: LinkCreate
+):
     ...
 
 
 @router.get("/@me", response_model=list[LinkRead])
-def get_current_links(user: str = Depends(get_user)):
+def get_current_links(user: str = Depends(get_user), db: Database = Depends(get_db)):
     ...
 
 
 @router.get("/@me/{id}", response_model=LinkRead)
-def get_current_link(user: str = Depends(get_user), *, id: str = Path(title="Link ID")):
+def get_current_link(
+    user: str = Depends(get_user),
+    db: Database = Depends(get_db),
+    *,
+    id: str = Path(title="Link ID")
+):
     ...
 
 
 @router.put("/@me/{id}", response_model=LinkRead)
 def update_current_link(
-    user: str = Depends(get_user), *, id: str = Path(title="Link ID"), link: LinkUpdate
+    user: str = Depends(get_user),
+    db: Database = Depends(get_db),
+    *,
+    id: str = Path(title="Link ID"),
+    link: LinkUpdate
 ):
     ...
 
 
 @router.delete("/@me/{id}", response_model=Message)
 def delete_current_link(
-    user: str = Depends(get_user), *, id: str = Path(title="Link ID")
+    user: str = Depends(get_user),
+    db: Database = Depends(get_db),
+    *,
+    id: str = Path(title="Link ID")
 ):
     ...
--- backend/app/app/api/api_v1/endpoints/profiles.py
+++ backend/app/app/api/api_v1/endpoints/profiles.py
@@ -1,21 +1,32 @@
 from fastapi import APIRouter, Depends, UploadFile
+from pymongo.database import Database
 
-from app.api.deps import get_user
+from app.api.deps import get_user, get_db
 from app.schemas.profile import ProfileRead, ProfileUpdate
 
 router = APIRouter()
 
 
 @router.get("/@me", response_model=ProfileRead)
-def get_current_profile(user: str = Depends(get_user)):
+def get_current_profile(user: str = Depends(get_user), db: Database = Depends(get_db)):
     ...
 
 
 @router.put("/@me", response_model=ProfileRead)
-def update_current_profile(user: str = Depends(get_user), *, profile: ProfileUpdate):
+def update_current_profile(
+    user: str = Depends(get_user),
+    db: Database = Depends(get_db),
+    *,
+    profile: ProfileUpdate
+):
     ...
 
 
 @router.put("/@me/picture", response_model=ProfileRead)
-def upload_current_picture(user: str = Depends(get_user), *, picture: UploadFile):
+def upload_current_picture(
+    user: str = Depends(get_user),
+    db: Database = Depends(get_db),
+    *,
+    picture: UploadFile
+):
     ...
