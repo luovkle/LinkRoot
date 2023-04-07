0d9c1f16c014b506ccc601805d11cbdf8d31ac35
luovkle <roberto.silva.dev@protonmail.com>
2023-04-06 18:55:37 -0600

    ✨ Add functionality to link endpoints

--- backend/app/app/api/api_v1/endpoints/links.py
+++ backend/app/app/api/api_v1/endpoints/links.py
@@ -4,6 +4,7 @@ from pymongo.database import Database
 from app.api.deps import get_user, get_db
 from app.schemas.link import LinkCreate, LinkRead, LinkUpdate
 from app.schemas.message import Message
+from app.crud.link import crud_link
 
 router = APIRouter()
 
@@ -12,12 +13,12 @@ router = APIRouter()
 def create_link(
     user: str = Depends(get_user), db: Database = Depends(get_db), *, link: LinkCreate
 ):
-    ...
+    return crud_link.create(db, user, link)
 
 
 @router.get("/@me", response_model=list[LinkRead])
 def get_current_links(user: str = Depends(get_user), db: Database = Depends(get_db)):
-    ...
+    return crud_link.read_many(db, user)
 
 
 @router.get("/@me/{id}", response_model=LinkRead)
@@ -27,7 +28,7 @@ def get_current_link(
     *,
     id: str = Path(title="Link ID")
 ):
-    ...
+    return crud_link.read_one(db, user, id)
 
 
 @router.put("/@me/{id}", response_model=LinkRead)
@@ -38,7 +39,7 @@ def update_current_link(
     id: str = Path(title="Link ID"),
     link: LinkUpdate
 ):
-    ...
+    return crud_link.update(db, user, id, link)
 
 
 @router.delete("/@me/{id}", response_model=Message)
@@ -48,4 +49,4 @@ def delete_current_link(
     *,
     id: str = Path(title="Link ID")
 ):
-    ...
+    return crud_link.delete(db, user, id)
---
+++ backend/app/app/crud/link.py
@@ -0,0 +1,51 @@
+from fastapi import HTTPException, status
+from fastapi.encoders import jsonable_encoder
+from pymongo.database import Database
+
+from app.schemas.link import Link, LinkCreate, LinkUpdate
+
+limit = 10
+
+
+class CRUDLink:
+    def _get_by_user(self, db: Database, user: str):
+        return list(db.links.find({"user": user}).limit(limit))
+
+    def _get_by_id(self, db: Database, user: str, id: str):
+        doc = db.links.find_one({"user": user, "_id": id})
+        if not doc:
+            raise HTTPException(status.HTTP_404_NOT_FOUND)
+        return doc
+
+    def _allow_new_doc(self, db: Database, user: str):
+        return False if db.links.count_documents({"user": user}) >= limit else True
+
+    def create(self, db: Database, user: str, link: LinkCreate):
+        if not self._allow_new_doc(db, user):
+            raise HTTPException(
+                status.HTTP_403_FORBIDDEN, "Maximum number of elements reached"
+            )
+        link_db = jsonable_encoder(Link.parse_obj(link))
+        id = db.links.insert_one({"user": user, **link_db}).inserted_id
+        return self._get_by_id(db, user, id)
+
+    def read_one(self, db: Database, user: str, id: str):
+        return self._get_by_id(db, user, id)
+
+    def read_many(self, db: Database, user: str):
+        return self._get_by_user(db, user)
+
+    def update(self, db: Database, user: str, id: str, link: LinkUpdate):
+        doc = self._get_by_id(db, user, id)
+        changes = db.links.update_one(
+            {"user": user, "_id": id}, {"$set": link.dict(exclude_none=True)}
+        ).modified_count
+        return self._get_by_id(db, user, id) if changes else doc
+
+    def delete(self, db: Database, user: str, id: str):
+        doc = self._get_by_id(db, user, id)
+        db.links.delete_one({"user": user, "_id": doc["_id"]})
+        return {"msg": "ok"}
+
+
+crud_link = CRUDLink()
